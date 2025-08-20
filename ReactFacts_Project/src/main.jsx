d51f2bcb17485733550988d0047a585b7b39847a
19parke <19parke@gmail.com>
2025-08-20 17:46:36 +0900

    custom component

--- ReactFacts_Project/src/main.jsx
+++ ReactFacts_Project/src/main.jsx
@@ -2,16 +2,39 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import reactLogo from './assets/react-logo.png'
 
-createRoot(document.getElementById('root')).render(
-  <div>
-    <img src={reactLogo} alt='react-logo' width="50px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first release in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 200K stars on GitHub</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
+// createRoot(document.getElementById('root')).render(
+//   <div>
+//     <img src={reactLogo} alt='react-logo' width="50px" />
+//     <h1>Fun facts about React</h1>
+//     <ul>
+//       <li>Was first release in 2013</li>
+//       <li>Was originally created by Jordan Walke</li>
+//       <li>Has well over 200K stars on GitHub</li>
+//       <li>Is maintained by Meta</li>
+//       <li>Powers thousands of enterprise apps, including mobile apps</li>
+//     </ul>
+//   </div>
+// )
+
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  // temporaryName()
+  <TemporaryName />
 )
+
+function TemporaryName(){
+  return (
+    <div>
+     <img src={reactLogo} alt='react-logo' width="50px" />
+     <h1>Fun facts about React</h1>
+     <ul>
+       <li>Was first release in 2013</li>
+       <li>Was originally created by Jordan Walke</li>
+       <li>Has well over 200K stars on GitHub</li>
+       <li>Is maintained by Meta</li>
+       <li>Powers thousands of enterprise apps, including mobile apps</li>
+     </ul>
+   </div>
+  )
+}
