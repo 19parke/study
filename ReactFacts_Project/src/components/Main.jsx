eff722263aab74e373b00b9ff0444bfeb9ae4045
19parke <19parke@gmail.com>
2025-09-05 17:29:10 +0900

    ReactFactsProject done

--- ReactFacts_Project/src/components/Main.jsx
+++ ReactFacts_Project/src/components/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main(){
     return (
-        <h1>Main goes here</h1>        
+        <main className="react-main-content">
+            <h1 className="main-header">Fun facts about React</h1>
+            <ul className="main-ul">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K starts on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
