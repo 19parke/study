a04324a87f1933476b6de46625c822d599136695
19parke <19parke@gmail.com>
2025-10-24 17:13:13 +0900

    header

--- advanced_react_practices/src/Header.js
+++ advanced_react_practices/src/Header.js
@@ -1,4 +1,9 @@
+import React from "react"
+import { ThemeContext } from "./App"
+
 export default function Header(){
+    const value = React.useContext(ThemeContext);
+    
     return (
         <header className="dark-theme">
             <h1>Dark theme</h1>
