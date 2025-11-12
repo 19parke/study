6f21966159f9b3fe63a7b5911d6c0ec3919519d6
19parke <19parke@gmail.com>
2025-11-12 17:17:20 +0900

    Routing

---
+++ routing/src/main.jsx
@@ -0,0 +1,30 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import About from './About.jsx';
+
+function App(){
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Home(){
+  return (
+    <h1>Hello, React Router!</h1>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+);
